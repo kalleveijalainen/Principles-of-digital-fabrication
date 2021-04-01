d5d4c1e41699d846cdbf3c50daf7fefdf40e7bcb
jkorkko <66636165+jkorkko@users.noreply.github.com>
2021-04-01 08:48:04 +0300

    indentation fixes and comments added

--- arduino.c
+++ arduino.c
@@ -1,6 +1,6 @@
-int sensePin = A0;  //This is the Arduino Pin that will read the sensor output
-int sensorInput;    //The variable we will use to store the sensor input
-double temp;        //The variable we will use to store temperature in degrees. 
+int sensePin = A0;
+int sensorInput;
+double temp; 
 int motorPin = A1;
 int speed = 200;
 int stop = 0;
@@ -11,30 +11,32 @@ void setup() {
  }
 
 void loop() {
-    // Measure temp and control motor in 1s cycles
-    //--------------------------------------------
-    // credit to @Chris
-    // https://bc-robotics.com/tutorials/using-a-tmp36-temperature-sensor-with-arduino/
-    sensorInput = analogRead(sensePin);
-    temp = (double)sensorInput / 1024;
-    temp = temp * 5;
-    temp = temp - 0.5;
-    temp = temp * 100;
-    //--------------------------------------------
-    if (temp > 21) {
-  	  runMotor();
-    }
-    else {
-      stopMotor();
-    }
-
+// Measure temp and control motor in 1s cycles
+//--------------------------------------------
+// credit to @Chris
+// https://bc-robotics.com/tutorials/using-a-tmp36-temperature-sensor-with-arduino/
+	sensorInput = analogRead(sensePin);
+	temp = (double)sensorInput / 1024;
+	temp = temp * 5;
+	temp = temp - 0.5;
+	temp = temp * 100;
+	//--------------------------------------------
+	if (temp > 21) {
+		runMotor();
+	}
+	else {
+		stopMotor();
+	}
+	//Prevent constant measuring
     delay(1000);
 }
 
 void runMotor() {
+	//Spin rotor
 	analogWrite(motorPin, speed);
 }
 
 void stopMotor() {
+	//Slow down rotor / keep it stopped
 	analogWrite(motorPin, stop);
 }
